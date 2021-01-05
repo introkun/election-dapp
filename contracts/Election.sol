2790f6513c288e1de8be46663f01bd9d3e492275
introkun <introkun@gmail.com>
2021-01-05 21:24:11 +0300

    step 2 list candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,24 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    function _addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        _addCandidate("Candidate 1");
+        _addCandidate("Candidate 2");
     }
 }
